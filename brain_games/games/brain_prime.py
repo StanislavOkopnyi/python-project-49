da0ffb7d7d5ec2d2e892f407ab1d4fa965782dd3
Pineapple_Juice <st.okopnyi@gmail.com>
2023-02-13 15:00:17 +0700

    Step 9
    Added brain-prime
    Updated README.md and pyproject.toml

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import brain_games.games.templates.conversation_templates as tmp
+import prompt
+from math import sqrt
+from random import randint
+
+
+def main():
+    name = tmp.greetings()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+    def right_answer(num: int) -> str:
+        if num in [1, 2]:
+            return "yes"
+        for div in range(2, int(sqrt(num) + 1)):
+            if num % div == 0:
+                return "no"
+        return "yes"
+
+    for _ in range(3):
+        number = randint(1, 100)
+        print(f"Question: {number}")
+        user_answer = prompt.string("Your answer: ")
+
+        if tmp.is_response_correct(user_answer, right_answer(number), name):
+            continue
+        else:
+            break
+    else:
+        tmp.win(name)
+
+
+if __name__ == "__main__":
+    main()
